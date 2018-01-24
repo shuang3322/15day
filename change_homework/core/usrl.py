0f6e37e6938e67bd8f42c5bc448fd07b05510a53
shuang3322 <shuang0528@hotmail.com>
2018-01-24 17:44:20 +0800

    Revert "Revert "功能流程完成""
    This reverts commit 4d82bd4cf0ee1fd96d57333917266aff8fa2dbd3.

--- change_homework/core/usrl.py
+++ change_homework/core/usrl.py
@@ -6,11 +6,11 @@ BASE_DIR=os.path.dirname(os.path.dirname(Path))
 sys.path.append(BASE_DIR)
 from core import base_lab
 from sqlalchemy.orm import sessionmaker
-class login():
+class login():#账户密码确定
     def __init__(self,name,passwd):
         self.name = name
         self.passwd = passwd
-    def return_live(self):
+    def return_levele(self):
         try:
             Session_class = sessionmaker(bind=base_lab.engine)  # 创建与数据库的会话session class ,注意,这里返回给session的是个class,不是实例
             Session = Session_class()
@@ -19,5 +19,54 @@ class login():
         except BaseException as Ever:
             print(Ever)
             return None
-run_login = login('shuang','123456')
-print("角色查询:",run_login.return_live())
+class UI_dict():#界面
+    def __init__(self,role):
+        self.studet_dict={
+            "Pay homework" : "Pay_homework",
+            "Check the grade" : "Check_the_grade",
+        }
+        self.teacher_dict= {
+            "Management class" : "Management_class",
+            "Add class record" : "Add_class_record",
+            "Change the grade" : "Change_the_grade",
+        }
+        self.role = role
+    def return_role_dict(self):
+        if self.role == "老师":
+            return self.teacher_dict
+        else:return self.studet_dict
+
+
+class Features():
+    def Pay_homework(self):
+        print("Features:","Pay_homework")
+
+    def Check_the_grade(self):
+        print("Features:","Check_the_grade")
+
+    def Management_class(self):
+        print("Features:","Management_class")
+
+    def Add_class_record(self):
+        print("Features:","Add_class_record")
+
+    def Change_the_grade(self):
+        print("Features:","Change_the_grade")
+
+test = login("shuang","123456")
+s =str(test.return_levele())
+# print("s",s,type(s))
+ui = UI_dict(s)
+ui_dict = ui.return_role_dict()
+for key,item in ui_dict.items():
+     print("**",key,"**",)
+     # print("key_:",key,"<======>","item_:",item)
+init_Features = input("输入功能").strip()
+feas = Features()
+print("init_Features :",init_Features,"ui_dict.get(init_Features) :",ui_dict.get(init_Features))
+print(hasattr(feas, ui_dict.get(init_Features)))
+if hasattr(feas, ui_dict.get(init_Features))is True:
+    go_fcun = getattr(feas, ui_dict.get(init_Features))
+    go_fcun()
+else:
+    print("输入错误：",init_Features)
